7bca9fbc89f2cc1a66545d1cc75a3a9280f7bdac
Himanshu Nagapure <himunagapure114@gmail.com>
2024-11-17 12:16:48 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -16,10 +16,13 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
+
+'''
+For testing exception.py
 if __name__=="__main__":
     try:
         a = 1/0
     except Exception as e:
         logging.info("Error: Divide by zero")
         raise CustomException(e, sys)
+'''    
